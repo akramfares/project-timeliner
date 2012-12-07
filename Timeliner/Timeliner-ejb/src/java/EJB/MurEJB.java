ab885ba49211375a450adfc64561fe5254839598
akramfares@gmail.com <akramfares@gmail.com@573ebe1b-b9f5-77a0-f699-bedf841af377>
2012-12-07 14:35:55 +0000

    Architecture de base

---
+++ Timeliner/Timeliner-ejb/src/java/EJB/MurEJB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EJB;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author akram
+ */
+@Stateful
+public class MurEJB implements MurEJBLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
---
+++ Timeliner/Timeliner-ejb/src/java/Entity/Utilisateur.java
@@ -0,0 +1,176 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author akram
+ */
+@Entity
+public class Utilisateur implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String nom;
+    @Column
+    private String prenom;
+    @Column
+    private String email;
+    @Column
+    private String motdepasse;
+    @Column
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date datenaissance;
+    @Column
+    private Boolean sexe;
+    @Column
+    private String adresse;
+
+    @ManyToOne
+    private List<Ami> amis;
+    @OneToOne
+    private Mur mur;
+    @OneToMany
+    private List<Message> messages;
+    @OneToMany
+    private List<Notification> notifications;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotdepasse() {
+        return motdepasse;
+    }
+
+    public void setMotdepasse(String motdepasse) {
+        this.motdepasse = motdepasse;
+    }
+
+    public Date getDatenaissance() {
+        return datenaissance;
+    }
+
+    public void setDatenaissance(Date datenaissance) {
+        this.datenaissance = datenaissance;
+    }
+
+    public Boolean getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(Boolean sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public List<Ami> getAmis() {
+        return amis;
+    }
+
+    public void setAmis(List<Ami> amis) {
+        this.amis = amis;
+    }
+
+    public Mur getMur() {
+        return mur;
+    }
+
+    public void setMur(Mur mur) {
+        this.mur = mur;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Utilisateur)) {
+            return false;
+        }
+        Utilisateur other = (Utilisateur) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity.Utilisateur[ id=" + id + " ]";
+    }
+    
+}
